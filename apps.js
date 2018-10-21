42ee90d2393e2d5c6c185071592bde38b75c517d
CodeSoftly <eclittlejohn@yahoo.com>
2018-10-21 03:42:45 -0500

    Fixed some CSS styles
    Improved layout for mobile and desktop

--- apps.js
+++ apps.js
@@ -3,7 +3,7 @@ var elements  = document.getElementsByClassName('fade-out');
 function fadingInElements() {
 for (let i = 0; i<elements.length; i++) {
 	elements[i].style.opacity = 1;
-	elements[i].style.transition = 'opacity 1.5s';
+	elements[i].style.transition = 'opacity 2.5s';
 };
 }
 
