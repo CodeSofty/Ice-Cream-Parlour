8452a9d5d00256c8b3ddbe9009882efd62737e72
CodeSoftly <eclittlejohn@yahoo.com>
2018-10-21 22:44:29 -0500

    Cleaned up mobile look and feel
    - Changed some styles to make mobile more appealing
    
    - Added functionality to mobile navigation
    
    - Added some comments to javascript file

--- apps.js
+++ apps.js
@@ -1,4 +1,11 @@
 var elements  = document.getElementsByClassName('fade-out');
+var open = document.getElementsByClassName('nav-icon')[0];
+var close = document.getElementsByClassName('nav-close')[0];
+var mobileNavigation = document.getElementsByClassName('mobile')[0];
+var deskNavigation = document.getElementsByClassName('nav')[0];
+var h1 = document.getElementsByTagName('h1')[0];
+
+// Animates the transitions for fading elements
 
 function fadingInElements() {
 for (let i = 0; i<elements.length; i++) {
@@ -7,6 +14,27 @@ for (let i = 0; i<elements.length; i++) {
 };
 }
 
+
+
+
 window.onload = function() {
 	fadingInElements();
 };
+
+// Opens mobile menu when icon is clicked
+
+open.addEventListener('click', function() {
+	mobileNavigation.style.display = 'block';
+	deskNavigation.style.display = 'none';
+	h1.style.display = 'none';
+	close.style.display = 'block';
+});
+
+// Closes mobile menu when the close icon is clicked
+
+close.addEventListener('click', function() {
+	mobileNavigation.style.display = 'none';
+	deskNavigation.style.display = 'block';
+	h1.style.display = 'block';
+	close.style.display = 'none';
+})
